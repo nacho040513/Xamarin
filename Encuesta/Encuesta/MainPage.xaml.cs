0ed17646d8e949a7e9beeffdae1985830b9835a2
nacho040513 <nacho040513@gmail.com>
2020-10-03 18:01:13 -0500

    primera version con page de login

---
+++ Encuesta/Encuesta/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Firebase;
+using Firebase.Database;
+using Firebase.Auth;
+using Encuesta;
+
+
+namespace Encuesta
+{
+
+    
+    public partial class MainPage : ContentPage
+    {
+
+        FirebaseAuth mAuth;
+
+        public MainPage()
+        {
+            InitializeComponent();
+   
+        }
+
+        public void Datos(object sender, EventArgs e) 
+        {
+            Button button = (Button)sender;
+            //string correo = this.correo.Text;
+
+        }
+
+        public bool IsSignIn()
+        {
+            var user = Firebase.Auth.FirebaseAuth.Instance.CurrentUser;
+            return user != null;
+        }
+
+        public async Task<string> LoginWithEmailAndPassword(string email, string password)
+        {
+            try
+            {
+                var user = await Firebase.Auth.FirebaseAuth.Instance.SignInWithEmailAndPasswordAsync(email, password);
+                var token = await user.User.GetIdTokenAsync(false);
+                return token.Token;
+            }
+            catch (FirebaseAuthInvalidUserException e)
+            {
+                e.PrintStackTrace();
+                return string.Empty;
+            }
+            catch (FirebaseAuthInvalidCredentialsException e)
+            {
+                e.PrintStackTrace();
+                return string.Empty;
+            }
+        }
+
+
+        void InitializeFirebase()
+        {
+            var app = FirebaseApp.InitializeApp(this);
+
+            if (app == null)
+            {
+                var options = new FirebaseOptions.Builder()
+
+                    .SetApplicationId("uber-clone-da636")
+                    .SetApiKey("AIzaSyBpBjZCW6lj0r9KbfZ0ymvpzDziJvaJeu4")
+                    .SetDatabaseUrl("https://uber-clone-da636.firebaseio.com")
+                    .SetStorageBucket("uber-clone-da636.appspot.com")
+                    .Build();
+
+                app = FirebaseApp.InitializeApp(this, options);
+                mAuth = FirebaseAuth.Instance;
+            }
+            else
+            {
+                mAuth = FirebaseAuth.Instance;
+            }
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
